4272ad60356c9173c6c9ae5eccf77d6c5a4d1704
luisaramicar11 <luisaf.ramirez.cardona11@gmail.com>
2024-11-14 18:39:47 -0500

    adding filter

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -5,4 +5,5 @@ export * from './Th';
 export * from './Tr';
 export * from './Link/Link';
 export * from './InputSelect/InputSelect';
+export * from './InputSearch/InputSearch';
 export * from './InputLogin/Input';
